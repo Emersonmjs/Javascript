35f4bd8cd70d73dbc1352b84aab0f3c36a664ede
Emersonmjs <140715893+Emersonmjs@users.noreply.github.com>
2024-03-26 12:00:40 -0300

    Aula 2

---
+++ Aula 2/script.js
@@ -0,0 +1,9 @@
+"use strict"
+
+const nome = "Emerson"
+let idade = 19
+var peso = 60
+
+console.log("Nome: " + nome)
+console.log("Idade : " + idade + " anos")
+console.log("Peso : " + peso + " Kg")
