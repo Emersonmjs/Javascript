3ca79e39511cd94e54d74585b6d663fb3a0a0aae
Emersonmjs <140715893+Emersonmjs@users.noreply.github.com>
2024-03-29 10:36:29 -0300

    Aula 9

---
+++ Aula 9/script.js
@@ -0,0 +1,11 @@
+"use strict"
+let n1 = 10
+console.log("Valor: "+n1)
+console.log("Acrescentado depois: "+(n1++))
+console.log("Atual: "+n1)
+console.log("Acrescentado antes: "+(++n1))
+console.log("Atual: "+n1)
+console.log("Decrementado depois: "+(n1--))
+console.log("Atual: "+n1)
+console.log("Decrementado antes: "+ (--n1))
+console.log("Atual: "+n1)
