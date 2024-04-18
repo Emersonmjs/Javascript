6a90c1143390bff62fdeeb9fa6e83e69d5c1e5d4
Emersonmjs <140715893+Emersonmjs@users.noreply.github.com>
2024-04-18 10:36:20 -0300

    Atualização da aula 41

--- Aula 41/script.js
+++ Aula 41/script.js
@@ -3,6 +3,7 @@ const cursosTodos = document.querySelectorAll(".curso")
 const cursoo = document.querySelector("#c1")
 const button = document.querySelector('#button')
 const cursos = ['HTML','CSS','JavaScript','PHP','React','MySQL']
+const btnCursoSelecionado = document.querySelector('#btnCursoSelecionado')
 
 
 cursos.map((elemento, index)=>{
@@ -10,7 +11,26 @@ cursos.map((elemento, index)=>{
     novoElemento.setAttribute('id', 'c'+(index+1))
     novoElemento.setAttribute('class','curso c1')
     novoElemento.innerHTML = elemento
+    const comandos = document.createElement('div')
+    comandos.setAttribute('class', 'comandos')
+
+    const rb = document.createElement('input')
+    rb.setAttribute('type','radio')
+    rb.setAttribute('name', 'rb_curso')
+
+    comandos.appendChild(rb)
+    novoElemento.appendChild(comandos)
     caixa1.appendChild(novoElemento)
+    
+    
 })
 
-
+btnCursoSelecionado.addEventListener('click', ()=>{
+    const radios = [...document.querySelectorAll('input[type=radio]')]
+    let RadioSelecionado= radios.filter((ele)=>{
+        return ele.checked
+    })
+    RadioSelecionado = RadioSelecionado[0]
+    const CursoSelecionado = RadioSelecionado.parentNode.previousSibling.textContent
+    alert("Curso selecionado: "+CursoSelecionado)
+})
