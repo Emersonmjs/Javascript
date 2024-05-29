7e63bea13b4825aacb67bac05292edbc6848e947
Emersonmjs <140715893+Emersonmjs@users.noreply.github.com>
2024-05-29 14:26:39 -0300

    Aula 77

--- Aula 77/assets/script.js
+++ Aula 77/assets/script.js
@@ -1,4 +1,6 @@
+const relogio_div = document.querySelector("#relogio")
 const data = new Date()
+
 
 // console.log(Date.now())
 
@@ -8,3 +10,17 @@ dia= dia<10?"0"+dia:dia
 var mes = data.getMonth()
 mes = mes<10?"0"+mes:mes
 console.log(`${dia}/${mes}/${data.getFullYear()}`)
+
+const relogio=()=>{
+    const data = new Date()
+    let hora = data.getHours()
+    hora = hora<10?"0"+hora:hora
+    let minuto = data.getMinutes()
+    minuto = minuto<10?"0"+minuto:minuto
+    let segundo = data.getSeconds()
+    segundo = segundo<10?"0"+segundo:segundo
+    let hora_completa = hora+":"+minuto+":"+segundo
+    relogio_div.innerHTML=hora_completa
+}
+
+const intervalo = setInterval(relogio,1000)
